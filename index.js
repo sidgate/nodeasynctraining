f8b29f9d6e47dd9f984dbe7f443bea7132145c67
Siddharth Gargate <sidgate@gmail.com>
2019-12-11 20:48:06 +0530

    promise tasks

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const util = require('./util');
-//TODO functions, lambda
+const r = require('request');
+const fs = require('fs')
+
+// TODO functions, lambda
 // TODO function class
 // TODO prototype function
 // TODO ES6 class function
@@ -7,7 +10,13 @@ const util = require('./util');
 // TODO function parameter
 // TODO setTimeout
 // TODO callback
+// TODO File handling
+// TODO Http request
 // TODO callback hell
 // TODO Promise
 // TODO Resolve/Reject
-// Event listener
+// TODO then/catch
+// TODO all
+// TODO async await
+// TODO Event listener
+
