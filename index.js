96a052ec2b67ac312df277fab1a6d4829960e1c3
Siddharth Gargate <sidgate@gmail.com>
2019-12-11 09:07:19 +0530

    todos

---
+++ index.js
@@ -0,0 +1,12 @@
+//TODO functions, lambda
+// TODO function class
+// TODO prototype function
+// TODO ES6 class function
+// TODO prototype function (lambda)
+// TODO function parameter
+// TODO setTimeout
+// TODO callback
+// TODO callback hell
+// TODO Promise
+// TODO Resolve/Reject
+//
