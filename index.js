52de7a36df75970e5636b28eab0f7636aaa5d963
Anis Mujavar <amq5820@Lainpuma558.local>
2019-12-12 17:49:00 +0530

    assignment

--- index.js
+++ index.js
@@ -2,71 +2,17 @@ const utilFn = require('./util-fn');
 const util = require('util')
 const r = require('request');
 const fs = require('fs')
+const rp = require('request-promise');
 
-
-// TODO functions, lambda
-
-function print(){
-    console.log('hello')
-}
-
-let print2 = ()=> console.log('hello2')
-
-let print3 = function(){
-    console.log('hello3')
-}
-
-function Person(name){
-this.name = name
-
-this.getName = function(){
-    return this.name;
-}
-}
-
-
-
-
-class Person2 {
-    constructor(name){
-        this.name = name;
-    }
-
-    getName(){
-        return this.name;
-    }
-}
-// TODO function as parameter
-
-function add(a,b) { return a+b}
-
-function subs(a,b) {return a-b}
-
-function calculator(a,b, op ){
-    return op(a,b)
-}
-
-//console.log(calculator(20,10,subs))
-
-// TODO setTimeout 
-function readFile(filename){
-    
-    let callback = function(){
-        console.log(filename)
-        utilFn.longRunningFn(3000)
-        console.log('done '+ filename)
-    }
-
-    setTimeout(callback)
-    
-}
-
-console.log('start')
 
 fs.readFile('temp.txt','utf-8', function(err, content){
     let urls = content.split('\n');
+    r.get(urls[0],res(urls));
+});
 
-    r.get(urls[0], function(err, response){
+//Task2
+const res = function(urls){
+    return function(err, response){
         let body = JSON.parse(response.body)
         console.log(body.CurrValue)
         if(true){
@@ -75,26 +21,52 @@ fs.readFile('temp.txt','utf-8', function(err, content){
                 console.log(body.CurrValue)
             });
         }
+    }
+}
+
+console.log('start')
+
+//Task1
+var promise = new Promise(function(resolve,reject){
+    fs.readFile('temp.txt','utf-8', function(err, content){
+        if(true)
+        {
+            resolve(content)
+        }
+        else
+        {
+            reject(err);
+        }
     });
-
 });
+promise.then((content)=>{
+    console.log(content);
+    let urls = content.split('\n');
+    r.get(urls[0],res(urls));
+})
 
 
+//assignment ====>
+var body;
+var promisReadfile = util.promisify(fs.readFile)
+
+promisReadfile('temp.txt','utf-8').then((content)=>{
+    urls = content.split('\n');
+
+    var jeson1 = rp.get(urls[0]);
+    var jeson2 = rp.get(urls[1]);
+
+     Promise.all([jeson1,jeson2]).then((response)=>{
+        response.forEach(data => {
+            body = JSON.parse(data);
+            console.log("final ans =>"+body.CurrValue);
+        })
+     })
+})
 
 console.log('end')
 
 
-// TODO closure
-// TODO File handling
-// TODO Http request
-// TODO callback hell
-// TODO Promise
-// TODO Resolve/Reject
-// TODO then/catch
-// TODO all
-// TODO Promisify
-// TODO async await
-// TODO Event loop
-// TODO Event listener
+
 
 
