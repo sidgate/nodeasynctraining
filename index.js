95c9c81f1221e91570039f58b7e3cbd501c141cf
Himanshu Thapar <htq5942@lainpuma522.springernature.com>
2019-12-12 15:41:48 +0530

    assignment

--- index.js
+++ index.js
@@ -1,83 +1,136 @@
 const utilFn = require('./util-fn');
 const util = require('util')
 const r = require('request');
+const rp = require('request-promise');
 const fs = require('fs')
 
 
 // TODO functions, lambda
 
-function print(){
-    console.log('hello')
-}
+// function print(){
+//     console.log('hello')
+// }
 
-let print2 = ()=> console.log('hello2')
+// let print2 = ()=> console.log('hello2')
 
-let print3 = function(){
-    console.log('hello3')
-}
+// let print3 = function(){
+//     console.log('hello3')
+// }
 
-function Person(name){
-this.name = name
+// function Person(name){
+// this.name = name
 
-this.getName = function(){
-    return this.name;
-}
-}
-
-
+// this.getName = function(){
+//     return this.name;
+// }
+// }
 
 
-class Person2 {
-    constructor(name){
-        this.name = name;
-    }
 
-    getName(){
-        return this.name;
-    }
-}
-// TODO function as parameter
 
-function add(a,b) { return a+b}
+// class Person2 {
+//     constructor(name){
+//         this.name = name;
+//     }
 
-function subs(a,b) {return a-b}
+//     getName(){
+//         return this.name;
+//     }
+// }
+// // TODO function as parameter
 
-function calculator(a,b, op ){
-    return op(a,b)
-}
+// function add(a,b) { return a+b}
 
-//console.log(calculator(20,10,subs))
+// function subs(a,b) {return a-b}
 
-// TODO setTimeout 
-function readFile(filename){
+// function calculator(a,b, op ){
+//     return op(a,b)
+// }
+
+// //console.log(calculator(20,10,subs))
+
+// // TODO setTimeout 
+// function readFile(filename){
     
-    let callback = function(){
-        console.log(filename)
-        utilFn.longRunningFn(3000)
-        console.log('done '+ filename)
-    }
+//     let callback = function(){
+//         console.log(filename)
+//         utilFn.longRunningFn(3000)
+//         console.log('done '+ filename)
+//     }
 
-    setTimeout(callback)
+//     setTimeout(callback)
     
-}
+// }
 
 console.log('start')
 
-fs.readFile('temp.txt','utf-8', function(err, content){
-    let urls = content.split('\n');
+// var urlCall = function (urls) {
+//         return function (err, response){
+//             let body = JSON.parse(response.body)
+//             console.log(body.CurrValue)
+            
+//                 if(true){
+//                     r.get(urls[1], function(err2, response2){
+//                     let body = JSON.parse(response2.body)
+//                     console.log(body.CurrValue)
+//                     });
+//             }
+//         }
+//     }
 
-    r.get(urls[0], function(err, response){
-        let body = JSON.parse(response.body)
+
+// fs.readFile('temp.txt','utf-8', function(err, content){
+//     let urls = content.split('\n');
+//     r.get(urls[0], urlCall(urls));
+
+// });
+
+
+// var promise = new Promise(function(resolve,reject){
+//         fs.readFile('temp.txt', 'utf-8', function(err,content){
+//             if(err){
+//                 reject(err);
+//                 return;
+//             }
+//             resolve(content);
+//         });
+        
+// });
+
+// var promReadFile = util.promisify(fs.readFile)
+//     promReadFile('temp.txt','utf-8').then((content) => {
+//         urls = content.split('\n');
+//         urls.map(function(url)) {
+//             return rp.get(url);
+//         })
+//     })
+
+// promise.then((content) => (
+//     console.log(content)
+// ))
+
+
+var promReadfile = util.promisify(fs.readFile)
+promReadfile('temp.txt','utf-8').then((content)=>{
+     urls = content.split('\n');
+     var prom1 = rp.get(urls[0]);
+     var prom2 = rp.get(urls[1]);
+    //  console.log(prmomise1);
+     prom1.then((response)=>{
+         body = JSON.parse(response)
         console.log(body.CurrValue)
-        if(true){
-            r.get(urls[1], function(err2, response2){
-                let body = JSON.parse(response2.body)
-                console.log(body.CurrValue)
-            });
-        }
-    });
+     })
+     prom2.then((response)=>{
+         body = JSON.parse(response)
+         console.log(body.CurrValue);
+     })
+})
 
-});
+
+
+
+
+
 
 
 
