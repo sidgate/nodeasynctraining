27ace73c33c4f8b529bd85329d9d98d0a4f37703
Siddharth Gargate <sidgate@gmail.com>
2019-12-12 18:09:12 +0530

    promise.all

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const utilFn = require('./util-fn');
 const util = require('util')
 const r = require('request');
 const fs = require('fs')
+const rp = require('request-promise')
 
 
 // TODO functions, lambda
@@ -78,19 +79,89 @@ fs.readFile('temp.txt','utf-8', function(err, content){
     });
 
 });
+// fs.readFile('temp.txt','utf-8', function(err, content){
+//      urls = content.split('\n');
+//     var body;
+//     r.get(urls[0],function(err, response){
+//         body = JSON.parse(response.body)
+//         console.log(body.CurrValue)
+        
+//     });
+
+//     r.get(urls[1], function(err2, response2){
+//         let body2 = JSON.parse(response2.body)
+//         console.log(body2.CurrValue)
+//         console.log(utilFn.toFloat(body.CurrValue) + utilFn.toFloat(body2.CurrValue) )
+//     });
+
+// });
+
+//  callback hell
+//  Promise
+//  Resolve/Reject
+//  then/catch
+
+// var promise = new Promise(function(resolve, reject){
+//    fs.readFile('temp.txt', 'utf-8', function(err, content){
+//     if(err){
+//         reject(err);
+//         return;
+//     }
+
+//     resolve(content);
+//    });
+// });
+
+var promiseReadFile = util.promisify(fs.readFile)
+promiseReadFile('temp.txt', 'utf-8').then((content)=>{
+    urls = content.split('\n');
+    
+    let promises = []
+    let promise1 = rp.get(urls[0])
+    let promise2 = rp.get(urls[1])
+
+ 
+
+    promises.push(promise1)
+    promises.push(promise2)
+
+    var allpromises = Promise.all(promises)
+    allpromises.then((result)=>{
+        let out = ""
+        for(i=0;i<result.length;i++){
+            out += JSON.parse(result[i]).CurrValue
+        }
+        
+        return Promise.resolve(out)
+    }).then((out)=>{
+        console.log(out)
+        return Promise.reject(new Error('eeeee'))
+    }).
+    then(()=>console.log('then'))
+    .catch(()=>console.log('catch'))
+
+    // let promises = urls.map(function(url){
+    //     return rp.get(url)
+    // }).map(function(promise){
+    //     return promise.then((body)=>{
+    //         return JSON.parse(body).CurrValue
+    //     })
+    // })
+
+    // Promise.all(promises).then((arr)=>{
+    //     console.log (arr[0] +" "+  arr[1])
+    // })
+
+
+
+})
+
+
 
 
 
 console.log('end')
 
-
-// TODO closure
-// TODO File handling
-// TODO Http request
-// TODO callback hell
-// TODO Promise
-// TODO Resolve/Reject
-// TODO then/catch
 // TODO all
 // TODO Promisify
 // TODO async await
