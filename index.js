06f4cea86c1b2d754f60694aaa53ca837e7f68a6
Siddharth Gargate <sidgate@gmail.com>
2019-12-12 09:22:44 +0530

    util rename

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const util = require('./util');
-const r = require('request');
-const fs = require('fs')
+const utilFn = require('./util-fn');
+const util = require('util')
+//const r = require('request');
+//const fs = require('fs')
 
 // TODO functions, lambda
 // TODO function class
 // TODO prototype function
 // TODO ES6 class function
 // TODO prototype function (lambda)
-// TODO function parameter
+// TODO function as parameter
 // TODO setTimeout
 // TODO callback
+// TODO closure
 // TODO File handling
 // TODO Http request
 // TODO callback hell
@@ -17,6 +19,12 @@ const fs = require('fs')
 // TODO Resolve/Reject
 // TODO then/catch
 // TODO all
+// TODO Promisify
 // TODO async await
+// TODO Event loop
 // TODO Event listener
 
+
+console.log('start')
+
+console.log('end')
