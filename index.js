73acab8e40bbf2b72b722cdcb593ab7ab282eef5
Vivek Bute <vbq5822@C02P30M9G3QN.local>
2019-12-12 12:34:19 +0530

    File added

--- index.js
+++ index.js
@@ -6,81 +6,100 @@ const fs = require('fs')
 
 // TODO functions, lambda
 
-function print(){
-    console.log('hello')
-}
+// function print(){
+//     console.log('hello')
+// }
 
-let print2 = ()=> console.log('hello2')
+// let print2 = ()=> console.log('hello2')
 
-let print3 = function(){
-    console.log('hello3')
-}
+// let print3 = function(){
+//     console.log('hello3')
+// }
 
-function Person(name){
-this.name = name
+// function Person(name){
+// this.name = name
 
-this.getName = function(){
-    return this.name;
-}
-}
-
-
+// this.getName = function(){
+//     return this.name;
+// }
+// }
 
 
-class Person2 {
-    constructor(name){
-        this.name = name;
-    }
 
-    getName(){
-        return this.name;
-    }
-}
-// TODO function as parameter
 
-function add(a,b) { return a+b}
+// class Person2 {
+//     constructor(name){
+//         this.name = name;
+//     }
 
-function subs(a,b) {return a-b}
+//     getName(){
+//         return this.name;
+//     }
+// }
+// // TODO function as parameter
 
-function calculator(a,b, op ){
-    return op(a,b)
-}
+// function add(a,b) { return a+b}
 
-//console.log(calculator(20,10,subs))
+// function subs(a,b) {return a-b}
 
-// TODO setTimeout 
-function readFile(filename){
+// function calculator(a,b, op ){
+//     return op(a,b)
+// }
+
+// //console.log(calculator(20,10,subs))
+
+// // TODO setTimeout 
+// function readFile(filename){
     
-    let callback = function(){
-        console.log(filename)
-        utilFn.longRunningFn(3000)
-        console.log('done '+ filename)
-    }
+//     let callback = function(){
+//         console.log(filename)
+//         utilFn.longRunningFn(3000)
+//         console.log('done '+ filename)
+//     }
 
-    setTimeout(callback)
+//     setTimeout(callback)
     
-}
+// }
 
 console.log('start')
 
-fs.readFile('temp.txt','utf-8', function(err, content){
-    let urls = content.split('\n');
+// let urlCallback = function(urls){
+//     return function(err, response){
+//         let body = JSON.parse(response.body)
+//         console.log(body.CurrValue)
+//         if(true){
+//             r.get(urls[1], function(err2, response2){
+//                 let body = JSON.parse(response2.body)
+//                 console.log(body.CurrValue)
+//             })
+//         }
+//     }
+// }
+// fs.readFile('temp.txt','utf-8', function(err, content){
+//     let urls = content.split('\n');
 
-    r.get(urls[0], function(err, response){
-        let body = JSON.parse(response.body)
-        console.log(body.CurrValue)
-        if(true){
-            r.get(urls[1], function(err2, response2){
-                let body = JSON.parse(response2.body)
-                console.log(body.CurrValue)
-            });
+//     r.get(urls[0],urlCallback(urls));
+        
+// });
+
+
+var promise = new Promise(function(resolve,reject){
+    fs.readFile('tmmp.txt','utf-8',function(err,content){
+        let urls = content.split('\n');
+        if(err)
+        {
+            reject(err);
         }
-    });
-
-});
-
-
-
+        else
+        {
+            resolve(content);
+        }
+    })
+}).then(function(){
+    r.get(urls[0], function(err2, response2){
+        let body = JSON.parse(response2.body)
+        console.log(body.CurrValue)
+})
 console.log('end')
 
 
@@ -96,5 +115,3 @@ console.log('end')
 // TODO async await
 // TODO Event loop
 // TODO Event listener
-
-
