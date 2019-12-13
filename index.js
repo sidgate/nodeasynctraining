a072bd8b9077c3d507d7c50daa51fdeb71885b31
Sourabh Pukale <spq5940@springernature.com>
2019-12-13 10:56:04 +0530

    my first commit

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const utilFn = require('./util-fn');
 const util = require('util')
 const r = require('request');
 const fs = require('fs')
-const rp = require('request-promise')
-const fsp = require('fs').promises
-
+var promise=require('promise');
+// var urls;
 
 // TODO functions, lambda
 
@@ -48,6 +47,8 @@ function calculator(a,b, op ){
     return op(a,b)
 }
 
+
+
 //console.log(calculator(20,10,subs))
 
 // TODO setTimeout 
@@ -65,11 +66,9 @@ function readFile(filename){
 
 console.log('start')
 
-fs.readFile('temp.txt','utf-8', function(err, content){
-    let urls = content.split('\n');
-
-    r.get(urls[0], function(err, response){
-        let body = JSON.parse(response.body)
+let urlCallback=function(urls){
+    return function(err,response){
+    let body = JSON.parse(response.body)
         console.log(body.CurrValue)
         if(true){
             r.get(urls[1], function(err2, response2){
@@ -77,93 +76,65 @@ fs.readFile('temp.txt','utf-8', function(err, content){
                 console.log(body.CurrValue)
             });
         }
-    });
+}
+}
+fs.readFile('temp.txt','utf-8', function(err, content){
+     urls = content.split('\n');
 
+    r.get(urls[0], urlCallback(urls));
+        // let body = JSON.parse(response.body)
+        // console.log(body.CurrValue)
+        // if(true){
+        //     r.get(urls[1], function(err2, response2){
+        //         let body = JSON.parse(response2.body)
+        //         console.log(body.CurrValue)
+        //     });
+        // }
+   
 });
-// fs.readFile('temp.txt','utf-8', function(err, content){
-//      urls = content.split('\n');
-//     var body;
-//     r.get(urls[0],function(err, response){
-//         body = JSON.parse(response.body)
-//         console.log(body.CurrValue)
-        
-//     });
-
-//     r.get(urls[1], function(err2, response2){
-//         let body2 = JSON.parse(response2.body)
-//         console.log(body2.CurrValue)
-//         console.log(utilFn.toFloat(body.CurrValue) + utilFn.toFloat(body2.CurrValue) )
-//     });
-
-// });
-
-//  callback hell
-//  Promise
-//  Resolve/Reject
-//  then/catch
-
-// var promise = new Promise(function(resolve, reject){
-//    fs.readFile('temp.txt', 'utf-8', function(err, content){
-//     if(err){
-//         reject(err);
-//         return;
-//     }
-
-//     resolve(content);
-//    });
-// });
-
-var promiseReadFile = util.promisify(fs.readFile)
-promiseReadFile('temp.txt', 'utf-8').then((content)=>{
-    urls = content.split('\n');
-    
-    let promises = []
-    let promise1 = rp.get(urls[0])
-    let promise2 = rp.get(urls[1])
-
-
-
- 
-
-    promises.push(promise1)
-    promises.push(promise2)
-
-    var allpromises = Promise.all(promises)
-    allpromises.then((result)=>{
-        let out = ""
-        for(i=0;i<result.length;i++){
-            out += JSON.parse(result[i]).CurrValue
-        }
-        
-        return Promise.resolve(out)
-    }).then((out)=>{
-        console.log(out)
-        return Promise.reject(new Error('eeeee'))
-    }).
-    then(()=>console.log('then'))
-    .catch(()=>console.log('catch'))
-
-    // let promises = urls.map(function(url){
-    //     return rp.get(url)
-    // }).map(function(promise){
-    //     return promise.then((body)=>{
-    //         return JSON.parse(body).CurrValue
-    //     })
-    // })
-
-    // Promise.all(promises).then((arr)=>{
-    //     console.log (arr[0] +" "+  arr[1])
-    // })
-
-
-
-})
 
 
 console.log('end')
 
-//  all
-//  Promisify
+
+var promise=new promise(function(resolve,reject){
+    // fs.readFile('temp.txt','utf-8',function(err,content){
+    //     if(err){
+    //         reject(err);
+    //         return;
+    //     }
+    //     return resolve(content);
+    // })
+
+    fs.readFile('temp.txt','utf-8', function(err, content){
+        urls = content.split('\n');
+   
+       r.get(urls[0], urlCallback(urls));
+
+    
+    if(err){
+                reject(err);
+                return;
+            }
+            return resolve(content);
+        })
+    
+    reject(new Error('some error'))
+});
+
+promise.then((content)=>{
+    console.log(content);
+})
+
+// TODO closure
+// TODO File handling
+// TODO Http request
+// TODO callback hell
+// TODO Promise
+// TODO Resolve/Reject
+// TODO then/catch
+// TODO all
+// TODO Promisify
 // TODO async await
 // TODO Event loop
 // TODO Event listener
